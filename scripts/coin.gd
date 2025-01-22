71db30992004b2d6b7f92790addd172c26f070c9
charlie275691502 <charlie275691502@gmail.com>
2025-01-22 22:28:44 +1100

    Audio

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @onready var game_manager: Node2D = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.AddPoint()
-	queue_free()
+	animation_player.play("PickUp")
